e13f05fdcbd7c6ab1d5f719be2b6a21fa3114eca
长安 <69743019+Moon-du@users.noreply.github.com>
2025-08-07 15:56:59 +0800

    Update jdy23.c

--- Core/Src/jdy23.c
+++ Core/Src/jdy23.c
@@ -34,28 +34,41 @@ void JDY23_CheckTimeout(void) {
 void JDY23_ProcessData(uint8_t *data, uint16_t size) {
     // 查找帧头
     for (int i = 0; i < size; i++) {
-        if (data[i] == 'T' && i + 15 < size && data[i + 15] == '\n') {
-            // 控制指令: TxxxRyyyPpppYyyy\n
-            char buffer[5] = {0};
+        if (data[i] == 'T') {
+            // 查找帧尾
+            int end = i + 1;
+            while (end < size && data[end] != '\n') {
+                end++;
+            }
+            if (end < size) {
+                // 提取命令字符串
+                char command[end - i + 1];
+                memcpy(command, &data[i], end - i);
+                command[end - i] = '\0';
 
-            // 解析油门
-            buffer[0] = data[i+1]; buffer[1] = data[i+2]; buffer[2] = data[i+3];
-            jdy23_cmd.throttle = atoi(buffer);
+                // 解析命令
+                char *token = strtok(command, "R");
+                if (token && token[0] == 'T') {
+                    jdy23_cmd.throttle = atoi(token + 1);
 
-            // 解析滚转
-            buffer[0] = data[i+5]; buffer[1] = data[i+6]; buffer[2] = data[i+7];
-            jdy23_cmd.roll = atoi(buffer);
+                    token = strtok(NULL, "P");
+                    if (token) {
+                        jdy23_cmd.roll = atof(token);
 
-            // 解析俯仰
-            buffer[0] = data[i+9]; buffer[1] = data[i+10]; buffer[2] = data[i+11];
-            jdy23_cmd.pitch = atoi(buffer);
+                        token = strtok(NULL, "Y");
+                        if (token) {
+                            jdy23_cmd.pitch = atof(token);
 
-            // 解析偏航
-            buffer[0] = data[i+13]; buffer[1] = data[i+14]; buffer[2] = data[i+15];
-            jdy23_cmd.yaw = atoi(buffer);
-
-            jdy23_cmd.last_update = HAL_GetTick();
-            return;
+                            token = strtok(NULL, "\n");
+                            if (token) {
+                                jdy23_cmd.yaw = atof(token);
+                            }
+                        }
+                    }
+                }
+                jdy23_cmd.last_update = HAL_GetTick();
+                return;
+            }
         }
     }
 }
@@ -67,3 +80,4 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
         HAL_UART_Receive_DMA(&huart2, jdy23_rx_buffer, JDY23_RX_BUFFER_SIZE);
     }
 }
+
